3c48e26c3a76ec39c69b9ea304154a5aa930d49c
franwanderley <franwanderley@outlook.com>
2021-08-11 10:34:29 -0300

    1️⃣  classe de dominio

--- src/main/java/com/cursospring/aulamc/resources/CategoriaResources.java
+++ src/main/java/com/cursospring/aulamc/resources/CategoriaResources.java
@@ -1,5 +1,10 @@
 package com.cursospring.aulamc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.cursospring.aulamc.domain.Categoria;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResources {
    
    @RequestMapping(method = RequestMethod.GET)
-   public String listar(){
-      return "Testando REST";
+   public List<Categoria> listar(){
+      Categoria cat1 = new Categoria(1, "informatica");
+      Categoria cat2 = new Categoria(2, "Escritorio");
+      List<Categoria> lista = new ArrayList<>();
+      lista.add(cat1);
+      lista.add(cat2);
+      return lista;
    }
 }
