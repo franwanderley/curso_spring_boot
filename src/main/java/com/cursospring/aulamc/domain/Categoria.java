f1afd8f7572d27fe6144d58d9b16cda22b8edadf
franwanderley <franwanderley@outlook.com>
2021-08-11 12:01:35 -0300

    ⚙⚙ configurando o banco de dados

--- src/main/java/com/cursospring/aulamc/domain/Categoria.java
+++ src/main/java/com/cursospring/aulamc/domain/Categoria.java
@@ -2,9 +2,17 @@ package com.cursospring.aulamc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
    private static final long serialVersionUID = 1L; 
 
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Integer id;
    private String nome;
 
@@ -39,6 +47,7 @@ public class Categoria implements Serializable {
       result = prime * result + ((id == null) ? 0 : id.hashCode());
       return result;
    }
+   @Override
    public boolean equals(Object obj){
       if(this == obj)
          return true;
